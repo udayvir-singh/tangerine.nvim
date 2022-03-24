0c360b5ffa5578b3dfff6be5cfbc0ad386f440bd
udayvir-singh <udayvir-singh@users.noreply.github.com>
2022-03-24 06:04:24 +0000

    auto: run build steps

--- lua/tangerine/fennel.lua
+++ lua/tangerine/fennel.lua
@@ -1,46 +1,47 @@
 local env = require("tangerine.utils.env")
 local fennel = {}
-local function format_path(path, ext, macro_path_3f)
-  _G.assert((nil ~= macro_path_3f), "Missing argument macro-path? on fnl/tangerine/fennel.fnl:14")
+local function format_path(path, ext, macro_3f)
+  _G.assert((nil ~= macro_3f), "Missing argument macro? on fnl/tangerine/fennel.fnl:14")
   _G.assert((nil ~= ext), "Missing argument ext on fnl/tangerine/fennel.fnl:14")
   _G.assert((nil ~= path), "Missing argument path on fnl/tangerine/fennel.fnl:14")
   local function _1_()
-    if macro_path_3f then
-      return (";" .. path .. "?/init-macros.fnl")
+    if macro_3f then
+      return "init-macros."
     else
-      return ""
+      return "init."
     end
   end
-  return (path .. "?." .. ext .. ";" .. path .. "?/init." .. ext .. _1_())
+  return (path .. "?." .. ext .. ";" .. path .. "?/" .. _1_() .. ext)
 end
-local function get_rtp(ext, macro_path_3f)
+local function get_path(ext, macro_path_3f)
   _G.assert((nil ~= macro_path_3f), "Missing argument macro-path? on fnl/tangerine/fennel.fnl:19")
   _G.assert((nil ~= ext), "Missing argument ext on fnl/tangerine/fennel.fnl:19")
-  local out = {format_path(env.get("source"), ext, macro_path_3f)}
-  do
-    local rtp = (vim.o.runtimepath .. ",")
-    for entry in rtp:gmatch("(.-),") do
-      local path = (entry .. "/fnl/")
-      if (1 == vim.fn.isdirectory(path)) then
-        table.insert(out, format_path(path, ext, macro_path_3f))
-      else
-      end
+  local out = {}
+  local source = env.get("source")
+  local rtps = (vim.o.runtimepath .. ",")
+  table.insert(out, format_path("./", ext, macro_path_3f))
+  table.insert(out, format_path(source, ext, macro_path_3f))
+  for entry in rtps:gmatch("(.-),") do
+    local path = (entry .. "/fnl/")
+    if (1 == vim.fn.isdirectory(path)) then
+      table.insert(out, format_path(path, ext, macro_path_3f))
+    else
     end
   end
   return table.concat(out, ";")
 end
+local orig = {path = package.path}
 fennel.load = function(_3fversion)
   local version = (_3fversion or env.get("compiler", "version"))
   local fennel0 = require(("tangerine.fennel." .. version))
-  do end (fennel0)["path"] = (fennel0.path .. get_rtp("fnl", false))
-  do end (fennel0)["macro-path"] = (fennel0["macro-path"] .. get_rtp("fnl", true))
+  fennel0.path = get_path("fnl", false)
+  fennel0["macro-path"] = get_path("fnl", true)
   return fennel0
 end
-local orig = {path = package.path}
 fennel["patch-path"] = function()
-  local targetdirs = get_rtp("lua", false)
-  local sourcedirs = format_path(env.get("target"), "lua", false)
-  do end (package)["path"] = (orig.path .. ";" .. targetdirs .. ";" .. sourcedirs)
+  local target = get_path("lua", false)
+  local source = format_path(env.get("target"), "lua", false)
+  package.path = (orig.path .. ";" .. target .. ";" .. source)
   return true
 end
 return fennel
